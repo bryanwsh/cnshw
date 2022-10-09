e6bd39b8e71fc395f46e54a4db4f1a5697e6f725
bryanwsh <2320601625@qq.com>
2022-10-09 16:10:46 +0400

    answers

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module http_server_week2
 
 go 1.19
+
+require github.com/golang/glog v1.0.0
--- http_server_week2/main.go
+++ http_server_week2/main.go
@@ -1,21 +1,54 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"io"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/golang/glog"
 )
 
+func answer4(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(200)
+	io.WriteString(res, "ok")
+}
 
-func healthz(w http.ResponseWriter, r *http.Request)  {
-	io.WriteString(w, "ok")
+func answer1(res http.ResponseWriter, req *http.Request) {
+	glog.V(2).Info("answer1 run")
+	for k, v := range req.Header {
+		res.Header().Set(k, v[0])
+	}
+	io.WriteString(res, "answer1")
+}
+
+func answer2(res http.ResponseWriter, req *http.Request) {
+	os.Setenv("VERSION", "123456")
+	version := os.Getenv("VERSION")
+	io.WriteString(res, version)
+}
+
+func answer3(res http.ResponseWriter, req *http.Request) {
+	req_ip := req.RemoteAddr
+	io.WriteString(res, "answer3")
+	statusCode := 200
+	res.WriteHeader(statusCode)
+	resHeader := res.Header()
+	resCode := res.Header().Get("Response Code")
+	glog.V(2).Info(resHeader)
+	glog.V(2).Info("Client IP:", req_ip, ", Response Code:", resCode,".")
 }
 
 func main() {
-	fmt.Print("HTTP Server Start")
-	//handle
-	http.HandleFunc("/healthz", healthz)
+	// flag.Parse()
+	flag.Set("v", "4")
+	glog.Info("HTTP Server Start")
+	// handler
+	http.HandleFunc("/answer1", answer1)
+	http.HandleFunc("/answer2", answer2)
+	http.HandleFunc("/answer3", answer3)
+	http.HandleFunc("/healthz", answer4)
 	// listen 80
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {

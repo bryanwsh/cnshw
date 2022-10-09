17acc8c242ec1cf0b73193713919baf836534a4c
bryanwsh <2320601625@qq.com>
2022-10-09 14:44:35 +0400

    HTTP Server Init

--- http_server_week2/main.go
+++ http_server_week2/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+
+func healthz(w http.ResponseWriter, r *http.Request)  {
+	io.WriteString(w, "ok")
+}
 
 func main() {
-	fmt.Print("HELLO CN")
+	fmt.Print("HTTP Server Start")
+	//handle
+	http.HandleFunc("/healthz", healthz)
+	// listen 80
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
